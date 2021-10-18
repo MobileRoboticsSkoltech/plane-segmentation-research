1e20053ea14bfa6ef4975604cb98c7fa542a3983
Pavel Mokeev <pav3l.mokeev@gmail.com>
2021-10-18 19:55:31 +0300

    refactored compress and decompress methods in FIC

--- src/algorithmForNN/compressUtils.py
+++ src/algorithmForNN/compressUtils.py
@@ -3,43 +3,85 @@ class FIC:
     def compress(input_list_of_integers: list) -> list:
         result = []
         for number in input_list_of_integers:
-            temp = []
             if number < (1 << 7):
-                temp.append(hex(number))
+                result.append((number).to_bytes(1, byteorder="little"))
             elif number < (1 << 14):
-                temp.append(hex((number & 0x7F) | 0x80))
-                temp.append(hex(number >> 7))
+                result.append(((number & 0x7F) | 0x80).to_bytes(1, byteorder="little"))
+                result.append((number >> 7).to_bytes(1, byteorder="little"))
             elif number < (1 << 21):
-                temp.append(hex((number & 0x7F) | 0x80))
-                temp.append(hex(((number >> 7) & 0x7F) | 0x80))
-                temp.append(hex(number >> 14))
+                result.append(((number & 0x7F) | 0x80).to_bytes(1, byteorder="little"))
+                result.append((((number >> 7) & 0x7F) | 0x80)).to_bytes(
+                    1, byteorder="little"
+                )
+                result.append((number >> 14).to_bytes(1, byteorder="little"))
             elif number < (1 << 28):
-                temp.append(hex((number & 0x7F) | 0x80))
-                temp.append(hex(((number >> 7) & 0x7F) | 0x80))
-                temp.append(hex(((number >> 14) & 0x7F) | 0x80))
-                temp.append(hex(number >> 21))
+                result.append(((number & 0x7F) | 0x80).to_bytes(1, byteorder="little"))
+                result.append((((number >> 7) & 0x7F) | 0x80)).to_bytes(
+                    1, byteorder="little"
+                )
+                result.append((((number >> 14) & 0x7F) | 0x80)).to_bytes(
+                    1, byteorder="little"
+                )
+                result.append((number >> 21).to_bytes(1, byteorder="little"))
             else:
-                temp.append(hex((number & 0x7F) | 0x80))
-                temp.append(hex(((number >> 7) & 0x7F) | 0x80))
-                temp.append(hex(((number >> 14) & 0x7F) | 0x80))
-                temp.append(hex(((number >> 21) & 0x7F) | 0x80))
-                temp.append(hex(number >> 28))
-
-            result.append(temp)
+                result.append(((number & 0x7F) | 0x80).to_bytes(1, byteorder="little"))
+                result.append((((number >> 7) & 0x7F) | 0x80)).to_bytes(
+                    1, byteorder="little"
+                )
+                result.append((((number >> 14) & 0x7F) | 0x80)).to_bytes(
+                    1, byteorder="little"
+                )
+                result.append((((number >> 21) & 0x7F) | 0x80)).to_bytes(
+                    1, byteorder="little"
+                )
+                result.append((number >> 28).to_bytes(1, byteorder="little"))
 
         return result
 
     @staticmethod
     def decompress(input_byte_array: list) -> list:
         result = []
+        position = 0
 
-        for item in input_byte_array:
-            temp = 0
-            for index, byte in enumerate(item):
-                byte_int = (int(byte, 16) & 0x7F) << 7 * index
-                temp += byte_int
-
-            result.append(temp)
+        while len(input_byte_array) > position:
+            byte_to_int = int.from_bytes(
+                input_byte_array[position], signed=True, byteorder="little"
+            )
+            position += 1
+            temp_int = byte_to_int & 0x7F
+            if byte_to_int >= 0:
+                result.append(temp_int)
+                continue
+            byte_to_int = int.from_bytes(
+                input_byte_array[position], signed=True, byteorder="little"
+            )
+            position += 1
+            temp_int |= (byte_to_int & 0x7F) << 7
+            if byte_to_int >= 0:
+                result.append(temp_int)
+                continue
+            byte_to_int = int.from_bytes(
+                input_byte_array[position], signed=True, byteorder="little"
+            )
+            position += 1
+            temp_int |= (byte_to_int & 0x7F) << 14
+            if byte_to_int >= 0:
+                result.append(temp_int)
+                continue
+            byte_to_int = int.from_bytes(
+                input_byte_array[position], signed=True, byteorder="little"
+            )
+            position += 1
+            temp_int |= (byte_to_int & 0x7F) << 21
+            if byte_to_int >= 0:
+                result.append(temp_int)
+                continue
+            byte_to_int = int.from_bytes(
+                input_byte_array[position], signed=True, byteorder="little"
+            )
+            position += 1
+            temp_int |= byte_to_int << 28
+            result.append(temp_int)
 
         return result
 
