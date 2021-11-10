ee2fe3f6d186a1ea287459c6355e73c26f5f0255
Pavel Mokeev <pav3l.mokeev@gmail.com>
2021-11-10 20:39:48 +0300

    now picked points after reloading windows marks red color

--- src/open3DTool/drawingUtils.py
+++ src/open3DTool/drawingUtils.py
@@ -7,10 +7,10 @@ def draw_point_cloud(point_cloud: o3d.geometry.PointCloud):
     o3d.visualization.draw_geometries_with_editing([point_cloud])
 
 
-def pick_points(point_cloud: o3d.geometry.PointCloud):
+def pick_points(main_point_cloud: o3d.geometry.PointCloud) -> list:
     vis = o3d.visualization.VisualizerWithEditing()
     vis.create_window()
-    vis.add_geometry(point_cloud)
+    vis.add_geometry(main_point_cloud)
     vis.run()
     vis.destroy_window()
     return vis.get_picked_points()
@@ -23,3 +23,9 @@ def draw_and_pick_points_function(point_cloud: o3d.geometry.PointCloud):
         picked_points_list = pick_points(point_cloud)
         assert len(picked_points_list) == 3
 
+        three_picked_points = point_cloud.select_by_index(picked_points_list)
+        three_picked_points.paint_uniform_color([1.0, 0, 0])
+        point_cloud = (
+            point_cloud.select_by_index(picked_points_list, invert=True)
+            + three_picked_points
+        )
