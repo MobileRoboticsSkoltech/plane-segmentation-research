8c3f7567145c8f1c585e7f5d01aa435af41fe01e
Pavel Mokeev <pav3l.mokeev@gmail.com>
2021-11-10 21:45:00 +0300

    fixed black

--- src/open3DTool/planeUtils.py
+++ src/open3DTool/planeUtils.py
@@ -13,7 +13,9 @@ def get_plane_equation(three_points: o3d.geometry.PointCloud) -> list:
     return [a, b, c, d]
 
 
-def get_distance_to_all_points(point_cloud: o3d.geometry.PointCloud, plane: list) -> np.ndarray:
+def get_distance_to_all_points(
+    point_cloud: o3d.geometry.PointCloud, plane: list
+) -> np.ndarray:
     numpy_point_cloud = np.asarray(point_cloud.points)
     ones_array = np.ones((numpy_point_cloud.shape[0], 1), dtype=np.float64)
     numpy_point_cloud = np.append(numpy_point_cloud, ones_array, axis=1)
@@ -22,4 +24,3 @@ def get_distance_to_all_points(point_cloud: o3d.geometry.PointCloud, plane: list
     distance = np.abs(numpy_point_cloud @ plane) / np.linalg.norm(plane)
 
     return distance
-
